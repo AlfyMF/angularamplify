dce95dc09460641f51f202449a391ef123cb50fc
AlfyMF <alfy.francis@experionglobal.com>
2024-02-26 12:06:50 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,3 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { TodosComponent } from './todos/todos.component';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet, TodosComponent],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   title = 'amplify-app';
-// }
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
