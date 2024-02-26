386865b21f21a1535800d8be8bace7f1f398c4a4
AlfyMF <alfy.francis@experionglobal.com>
2024-02-26 15:10:48 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Amplify } from 'aws-amplify';
 import amplifyconfig from './amplifyconfiguration.json';
-
+Amplify.configure(amplifyconfig);
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-  Amplify.configure(amplifyconfig);
+
